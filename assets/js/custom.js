cff565845b5dfb6e0a93e967fce847d3855fae3f
Subash Rohith <subashrohith45@gmail.com>
2024-02-13 16:36:48 +0530

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,41 @@
+
+new WOW().init();
+
+
+  $(".sub-menu ul").hide();
+  $(".sub-menu a").click(function () {
+      $(this).parent(".sub-menu").children("ul").slideToggle("100");
+      $(this).find(".right").toggleClass("fa-angle-up fa-angle-down");
+  });
+      
+
+    // Get the button
+    let mybutton = document.getElementById("myBtn");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () {
+        scrollFunction();
+    };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+
+
+    
+
+    // Meanmenu
+$('.mean-menulist').meanmenu({
+meanMenuContainer: '.mobile-menu',
+
+});
